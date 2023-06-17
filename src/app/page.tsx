9274e03199fbbc61a8880280039dd586c74569cb
Leiwis Bernal <leiwis.bernal@externos-cl.cencosud.com>
2023-06-16 23:05:13 -0400

    add styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,10 +14,23 @@ import Image from "next/image";
 export default function Home() {
   const imageList = [zero, one, two, three, four, five, six, seven, eight];
   return (
-    <Grid container>
+    <Grid container style={{ justifyContent: "center" }} spacing={2}>
       {imageList.map((img, k) => (
-        <Grid item xs={12} sm={12} md={12} lg={12} key={k}>
-          <Image src={img} width={400} alt={`imagen de cagalogo ${k}`} />
+        <Grid
+          item
+          xs={12}
+          sm={12}
+          md={12}
+          lg={12}
+          key={k}
+          // style={{ display: "flex", justifyContent: "center" }}
+        >
+          <Image
+            src={img}
+            //  width={400}
+            alt={`imagen de catalogo ${k}`}
+            style={{ maxWidth: "100%", height: "100%" }}
+          />
         </Grid>
       ))}
     </Grid>
